e9d9ac7c2d784217458adaef6d57fbcb5ca5b358
abisola <abisola.ca@gmail.com>
2021-06-15 21:56:36 +0100

    fixed dropdown menu

--- index.js
+++ index.js
@@ -39,68 +39,17 @@ const moveSlide = (nextSlide) => {
 
 const showMenu = () => {
   let menu = document.getElementById("menu");
+  let hamburger = document.querySelector(".hamburger");
+  let close = document.querySelector(".close");
 
   if (menu.style.opacity == 0) {
     menu.setAttribute("style", "opacity: 1;");
-  } else menu.setAttribute("style", "opacity: 0;");
+    hamburger.classList.add("hide");
+    close.classList.add("show");
+  } else {
+    close.classList.remove("show");
+    hamburger.classList.remove("hide");
+    menu.setAttribute("style", "opacity: 0;");
+  }
 };
 
-// let slides = [...document.querySelectorAll(".testimonial")];
-// let currentSlide = 0;
-// const nextSlide = () => {
-//   // move current slide to the left
-//   // move next slide to the left
-//   const nextSlide = currentSlide === slides.length - 1 ? 0 : currentSlide + 1;
-//   if (nextSlide === 0) {
-//     [...slides].reverse().forEach((slide, index) => {
-//       if (index === slides.length - 1) {
-//         slide.classList.add("active");
-//       } else {
-//         slide.classList.remove("active");
-//         slide.classList.remove("slide-out-left");
-//       }
-//     });
-//   } else {
-//     slides[currentSlide].classList.add("slide-out-left");
-//     slides[currentSlide].classList.remove("active");
-//     slides[nextSlide].classList.add("active");
-//   }
-
-//   currentSlide = nextSlide;
-// };
-
-// const prevSlide = () => {
-//   const prevSlide = currentSlide === 0 ? slides.length - 1 : currentSlide - 1;
-
-//   if (prevSlide === slides.length - 1) {
-//     [...slides].reverse().forEach((slide, index) => {
-//       if (index === 0) {
-//         slide.classList.add("active");
-//       } else {
-//         slide.classList.remove("active");
-//         slide.classList.remove("slide-out-right");
-//         slide.classList.add("slide-out-left");
-
-//       }
-//     });
-//   } else {
-//     slides[currentSlide].classList.add("slide-out-right");
-//     slides[currentSlide].classList.remove("active");
-//     slides[prevSlide].classList.add("active");
-//   }
-
-//   currentSlide = prevSlide;
-// };
-
-// // radios.forEach((radio, index) => {
-// //     radio.addEventListener("click", () => {
-// //        if(index===currentSlide) {
-// //            slides[currentSlide].classList.add("active")
-// //            radio.classList.add("active")
-// //            currentSlide++
-// //        }
-// //        else {
-
-// //        }
-// //     })
-// // })
